0a18963174d554e4584f969c18285c6407904735
Anas <139791554+anasss09@users.noreply.github.com>
2025-09-07 15:39:45 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'https://renderdeploy-d5n0.onrender.com/',
+    baseURL: 'https://renderdeploy-d5n0.onrender.com',
     // baseURL: 'http://localhost:4444',
     withCredentials: true
 });
